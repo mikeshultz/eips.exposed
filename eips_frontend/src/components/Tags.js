df52d4ee2dfbf151d60a1dc966c4d5c50477247f
Mike Shultz <shultzm@gmail.com>
2019-05-25 23:12:50 -0600

    lowered base font size for tag list

--- eips_frontend/src/components/Tags.js
+++ eips_frontend/src/components/Tags.js
@@ -1,15 +1,17 @@
 import React from 'react';
 import Tag from './Tag';
 
+const BASE_FONT_SIZE = 10;
+
 class Tags extends React.Component {
   render () {
     const tags = this.props.tags ?
       this.props.tags.map(tag => (
         <Tag
-            key={tag.tagName}
-            tag={tag.tagName}
-            fontSize={tag.eipsCount ? 12 + tag.eipsCount : 12}
-            />
+          key={tag.tagName}
+          tag={tag.tagName}
+          fontSize={tag.eipsCount ? BASE_FONT_SIZE + tag.eipsCount : BASE_FONT_SIZE}
+          />
     )) : [];
     return (
       <div className="tags">
