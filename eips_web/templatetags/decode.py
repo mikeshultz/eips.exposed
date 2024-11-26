f4ba5b548f1753f52eed95104df3cf42d4ff3db7
Mike Shultz <shultzm@gmail.com>
2024-11-25 18:16:30 -0700

    chore: cleanup, remove unnecessary filter

--- eips_web/templatetags/decode.py
+++
@@ -1,9 +0,0 @@
-from django import template
-
-register = template.Library()
-
-
-@register.filter(name="decode")
-def decode_filter(value: bytes):
-    """Decode a utf-8 byte string."""
-    return value.decode("utf-8")
