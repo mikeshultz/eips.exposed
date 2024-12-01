12e339743bc7bd9781088a9aa25f0dc81967f521
Mike Shultz <shultzm@gmail.com>
2024-12-01 11:24:25 -0700

    fix: fix fixlinks filter, prevent matching full URLs and fix relative handling

--- eips_web/templatetags/fixlinks.py
+++ eips_web/templatetags/fixlinks.py
@@ -13,8 +13,28 @@ def fixlinks_filter(value: str):
         return matchobj.group(0).replace("/assets/", "/static/assets/")
 
     def fix_link(matchobj: re.Match) -> str:
-        return matchobj.group(0).replace(".md", ".html")
+        matched = matchobj.group(0)
 
-    fixed_links = re.sub(r"(eip|erc)\-[0-9]+.md", fix_link, value)
+        # NOTE: This is kind of a circuit breaker indicating it matched part of
+        #       a full URL.
+        # TODO: Make this more robust. Maybe update the regex to negative
+        #       lookbehind for http://?
+        if matched.startswith("/"):
+            return matched
+
+        if matched.startswith("./"):
+            matched = matched.replace("./", "")
+
+        if matchobj.group(1) == "erc":
+            prefix = "/ercs/"
+        else:
+            prefix = "/eips/"
+
+        rewrite = matched.replace(".md", ".html")
+        fin = f"{prefix}{rewrite}"
+        return fin
+
+    # (?<!http:\/\/)
+    fixed_links = re.sub(r"[\./]*(eip|erc)\-[0-9]+.md", fix_link, value)
     fixed_assets = re.sub(r"/assets/", fix_asset, fixed_links)
     return fixed_assets
